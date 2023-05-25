582265e9893e6d433cd5e51245a72b27f2b50c19
IoIxD <alphaproject217@gmail.com>
2023-05-24 19:24:54 -0700

    initial commit

---
+++ app/src/main/cpp/vipdroid.cpp
@@ -0,0 +1,20 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("vipdroid");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("vipdroid")
+//      }
+//    }
+
+#include <vips/vips8>
+
